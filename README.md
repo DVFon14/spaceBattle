7104d512df2f443b5ba4416f1a6fde0e32e5c8aa
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-11-07 18:30:40 -0700

    enemyShipClass created

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spaceBattle
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+console.log(getRandomArbitrary(3,6))
--- spaceBattle.js
+++ spaceBattle.js
@@ -7,7 +7,7 @@ console.log(
 );
 
 console.log(
-  "The odds are in your favor, as your Hull is a solild 20, while theirs is between 3 and 6;"
+  "The odds are in your favor, as your Hull is a solid 20, while theirs is between 3 and 6;"
 );
 console.log("your firepower is 5, while theirs is between 2 and 4;");
 console.log("and your accuracy is 0.7, while theirs is between 0.6 and 0.8");
@@ -18,6 +18,7 @@ console.log(
 
 
 
+const { get } = require("https");
 let {fightOptions} = require("./fightOptions.js");
 
 let playGame = (userInput) => {
@@ -28,3 +29,38 @@ let playGame = (userInput) => {
 process.stdin.on("data", playGame);
 
 
+const ourShip = {
+    hull: 20,
+    firepower: 5,
+    accurarcy: 0.7
+}
+
+// const e_hull = Math.random() * 3 + 3;
+// console.log(e_hull)
+
+
+// function e_firepower(min, max) {
+//     return Math.random() * (max - min) + min;
+//   }
+
+// console.log(e_hull(3,6))
+
+
+
+class enemyShipClass {
+  constructor () {
+    this.e_hull = Math.random() * 3 + 3;
+    this.e_firepower = Math.random() * 2 + 2;
+    this.accuracy = Math.random() * 0.2 + 0.6;
+  }
+  // generateCar () {
+  //   const newCar = new Car(this.company, this.cars.length); //instantiating a new object
+  //   this.cars.push(newCar);
+  // }
+  // findCar (index) {
+  //   return this.cars[index]; //this.cars[1]
+  // }
+}
+
+const enemyShip1Instance = new enemyShipClass()
+console.log(enemyShip1Instance)
